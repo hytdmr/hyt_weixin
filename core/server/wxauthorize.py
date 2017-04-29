3fcdc952c6e8f4b1cafab95df2cafc50c9404c4f
hyt <hytysu@163.com>
2017-04-29 10:53:51 +0800

    add post to wxauthorize

--- core/server/wxauthorize.py
+++ core/server/wxauthorize.py
@@ -7,6 +7,8 @@
 from core.logger_helper import logger
 import hashlib
 import tornado.web
+import time
+import xml.etree.cElementTree as ET
 
 
 class WxSignatureHandler(tornado.web.RequestHandler):
@@ -46,3 +48,74 @@ check_signature:校验signature是否正确
         sha1 = hashlib.sha1(s.encode('utf-8')).hexdigest()
         logger.debug('sha1=' + sha1 + '&signature' + signature)
         return sha1 == signature
+
+    # def post(self):
+    #     body = self.request.body
+    #     logger.debug('微信消息回复中心】收到用户消息' + str(body.decode('utf-8')))
+    #     data = ET.fromstring(body)
+    #     ToUserName = data.find('ToUserName').text
+    #     FromUserName = data.find('FromUserName').text
+    #     MsgType = data.find('MsgType').text
+    #     if MsgType == 'event':
+    #         '''接收事件推送'''
+    #         try:
+    #             Event = data.find('Event').text
+    #             if Event == 'subscribe':
+    #                 CreateTime = int(time.time())
+    #                 reply_content = '欢迎关注我的微信公众号'
+    #                 out = self.reply_text(
+    #                     FromUserName, ToUserName, CreateTime, reply_content)
+    #                 self.write(out)
+
+    #         except Exception as e:
+    #             pass
+
+    def reply_text(self, FromUserName, ToUserName, CreateTime, Content):
+        '''回复文本消息模板'''
+        textTpl = """< xml > < ToUserName > <![CDATA[% s]] > </ToUserName >
+                     < CreateTime > %s < /CreateTime >
+                     < MsgType > <![CDATA[ % s]] > </MsgType >
+                     < Content > <![CDATA[% s]] > </Content ></xml >"""
+        out = textTpl % (FromUserName, ToUserName, CreateTime, 'text', Content)
+        return out
+
+    def post(self):
+        body = self.request.body
+        logger.debug('微信消息回复中心】收到用户消息' + str(body.decode('utf-8')))
+        data = ET.fromstring(body)
+        ToUserName = data.find('ToUserName').text
+        FromUserName = data.find('FromUserName').text
+        MsgType = data.find('MsgType').text
+        if MsgType == 'text' or MsgType == 'voice':
+            '''文本消息或者语音消息'''
+            try:
+                MsgId = data.find('MsgId').text
+                if MsgId == 'text':
+                    Content = data.find('Content').text
+                elif MsgType == 'voice':
+                    Content = data.find('Recognition').text
+                if Content == u'你好':
+                    reply_content = '您好，请问有什么可以帮助您的吗？'
+                else:
+                    reply_content = '客服小儿智商不够用啦~'
+                if reply_content:
+                    CreateTime = int(time.time())
+                    out = self.reply_text(
+                        FromUserName, ToUserName, CreateTime, Content)
+                    self.write(out)
+            except Exception as e:
+                pass
+
+        elif MsgType == 'event':
+            '''接收事件推送'''
+            try:
+                Event = data.find('Event').text
+                if Event == 'subcribe':
+                    '''关注事件'''
+                    CreateTime = int(time.time())
+                    reply_content = self.sys_order_reply
+                    out = self.reply_text(
+                        FromUserName, ToUserName, CreateTime, Content)
+                    self.write(out)
+            except Exception as e:
+                pass
